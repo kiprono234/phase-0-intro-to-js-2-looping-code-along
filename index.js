e7a952bdf8dc4c7fc3d857c33fb8c48ebb9c8ca6
kevin <kipronok234@gmail.com>
2024-06-30 23:07:33 +0300

    index.js looping

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+function writeCards(names,event) {
+    return names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`);
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+ 
